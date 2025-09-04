implement numerical hessian for surrogate and surface object fix a bug on scale_momentum in FSSH object fix a random seed bug on Wigner sampling scheme in sample object

dynamics.py
@@ -205,8 +205,8 @@ class FSSH(Dynamics):
                            'p': propagator.y[self.nc:2*self.nc].real}
                 traj.update(tupdate)
                 self.state = traj.state()
-                print(f"state:{self.state}")
-                print(f"time:{traj.t()/41.334}")
+                # print(f"state:{self.state}")
+                # print(f"time:{traj.t()/41.334}")
                 propagator.step()
 
             # if we got here because the propagator failed not b/c
@@ -265,7 +265,7 @@ class FSSH(Dynamics):
         pop    = np.diag(dm)
         t_prob = np.array([max(0., dt*b[j, s]/pop[s])
                             for j in range(self.ns)])
-        # t_prob[s] = 0.
+        t_prob[s] = 0.
 
         # check whether or not to hop
         r    = np.random.uniform()
@@ -276,9 +276,6 @@ class FSSH(Dynamics):
             if r < prob:
                 return int(st)
             st += 1
-        print(f"r:{r}")
-        print(f"prob:{prob}")
-        print(f"prop:{pop}")
         return int(s)
 
     #
@@ -302,7 +299,7 @@ class FSSH(Dynamics):
 
         delta = b**2 - 4*a*c
 
-        print(f"delta:{delta.shape}")
+        # print(f"delta:{delta}")
 
         # if discriminant less than zero: frustrated hop,
         # no adjustment that conserves energy
@@ -314,7 +311,10 @@ class FSSH(Dynamics):
         else:
             gamma = (-b + np.sign(b)*np.sqrt(delta))/(2*a)
 
-        delta_p = gamma * nac / self.m
+        delta_p = gamma * nac
+
+        print(f"delta p:{delta_p}")
+
 
         return delta_p
 

sample.py
@@ -10,14 +10,14 @@ class Sample(ABC):
 
     def __init__(self):
         super().__init__()
-    
+
     @abstractmethod
     def sample(self):
         pass
 
 class Wigner(Sample):
     """
-    Generate position and momenta drawn from a 
+    Generate position and momenta drawn from a
     Wigner distribution. Currently only works in cartesian
     coordinates
     """
@@ -28,15 +28,16 @@ class Wigner(Sample):
         if self.crd == 'cart':
             self.dim = ref_gm.x.shape[0]
         elif self.crd == 'intc':
-            print('Wigner sampling not currently implemented ' + 
+            print('Wigner sampling not currently implemented ' +
                   'for internal coordinates')
             os.abort()
-        else: 
+        else:
             print('crd='+str(crd)+' not recognized. Exiting...')
-            os.abort() 
+            os.abort()
 
-        self.ref_gm = ref_gm 
-        rseed       = np.random.default_rng(seed=seed)
+        self.ref_gm = ref_gm
+        np.random.seed(seed)  # set  seed for reproducibility
+
 
     #
     def update_origin(self, ref_gm):
@@ -57,7 +58,7 @@ class Wigner(Sample):
 
         if np.any([omega])== None or np.any([modes]) == None:
             return None
-        
+
         alpha = 0.5*omega
         sigma_x = np.sqrt(0.25 / alpha)
         sigma_p = np.sqrt(alpha)
@@ -96,8 +97,8 @@ class Wigner(Sample):
                         dist_p[ipass,:] = np.dot(modes, dp) * np.sqrt(masses)
 
                 if ipass < nsample:
-                    dx = np.random.normal(0., sigma_x, (nsample, nc))
-                    dp = np.random.normal(0., sigma_p, (nsample, nc))
+                    dx = self.rseed.normal(0., sigma_x, (nsample, nc))
+                    dp = self.rseed.normal(0., sigma_p, (nsample, nc))
 
             dist_x += self.ref_gm.x
             dist_p += self.ref_gm.p
@@ -141,7 +142,7 @@ class LHS(Sample):
                                       optimization = None,
                                       seed = rseed)
 
-    # 
+    #
     def update_origin(self, ref_gm):
         """
         update the ref_gm object
@@ -155,7 +156,7 @@ class LHS(Sample):
         in the negative/positive directions by scale[0]/scale[1]
         """
         d = [-1., 1.]
-        return np.array([disp*scale[i]*d[i] 
+        return np.array([disp*scale[i]*d[i]
                                for i in range(2)], dtype=float).T
 
     #
@@ -170,19 +171,19 @@ class LHS(Sample):
         # rescale points to the approproate bounds
         rnge = np.array([(bounds[i,1] - bounds[i,0])
                           for i in range(self.dim)], dtype=float)
-        # disps is the displacements in internal coordinates 
+        # disps is the displacements in internal coordinates
         # shape = [npts, ncoords]
         disps = bounds[:,0] + pts * rnge
 
-        # if cartesian is True and the displacements are in 
+        # if cartesian is True and the displacements are in
         # internals, convert to cartesians
         gms = None
         nd  = disps.shape[0]
         if self.crd == 'intc':
             if cartesian:
-                cdisps, fail = self.ref_gm.c2int.dint2cart(self.ref_gm.x, 
+                cdisps, fail = self.ref_gm.c2int.dint2cart(self.ref_gm.x,
                                                                   disps)
-                gms = self.ref_gm.x + cdisps[list(set([i for i in 
+                gms = self.ref_gm.x + cdisps[list(set([i for i in
                                                range(nd)]) - set(fail))]
             else:
                 gms = self.ref_gm.qx + disps
@@ -191,5 +192,3 @@ class LHS(Sample):
 
         # return the geometries
         return gms
-
-

surface.py
@@ -723,29 +723,46 @@ class ChemPotPy(Surface):
     #
     def hessian(self, gms, states = None):
         """
-        evaluate the hessian on states 'states'. If states=None, return
-        hessian for all defined states
+        compute the hessian by gradient differences
         """
-        if states == None:
-            states = [i for i in range(self.nstates)]
-        elif max(states) > self.nstates:
-            print('surface only defined for ' +str(self.nstates) +
-                   ': Exiting...')
-            os.abort()
 
-        nst = len(states)
-        nat = len(self.atms)
-        ngm = gms.shape[0]
-        hessian  = np.zeros((nst, ngm, 3*nat, 3*nat), dtype=float)
-        for i in range(ngm):
-            for j in range(natm):
-                gm  = self._chempotpygeom(gms[i,:] / self.gconv)
-                cppsurf = chempotpy.pg(self.molecule, self.surface, gm)
-                hessian[:,i,:,:] = np.reshape(cppsurf[1][[states]],
-                                                   (nst, 3*nat, 3*nat))
+        if states is None:
+            eval_st = list(range(self.nstates))
+        else:
+            eval_st = states
 
-        return hessian
+        delta = 1e-4
+        ng = gms.shape[0]  # number of geometries
+        nc = gms.shape[1]  # number of coordinates
+        nstates = len(eval_st)
 
+        hessall = np.zeros((nstates, ng, nc, nc), dtype=float)
+
+        for i in range(ng):
+            # gms[i,:] shape: (nc,)
+            for k in range(nc):
+                # Prepare displaced geometries for plus and minus displacement
+                disp_plus = np.array(gms[i,:], copy=True)
+                disp_minus = np.array(gms[i,:], copy=True)
+
+                disp_plus[k] += delta
+                disp_minus[k] -= delta
+
+                # Get gradients at displaced points for all eval_st states
+                # Assuming self.gradient returns shape: (nstates, nc)
+                p_grad = self.gradient(disp_plus.reshape(1, -1), states=eval_st)  # shape (nstates, nc)
+                m_grad = self.gradient(disp_minus.reshape(1, -1), states=eval_st)  # shape (nstates, nc)
+
+                # Central difference to approximate second derivative w.r.t coordinate k
+                # For each state, calculate second derivative matrix element for k-th column
+                # hessall[:, i, :, k] = (p_grad - m_grad) / (2 * delta)
+                hessall[:, i, :, k] = (p_grad - m_grad) / (2 * delta)
+
+            # Symmetrize Hessian for each state and geometry
+            for s in range(nstates):
+                hessall[s, i] = 0.5 * (hessall[s, i] + hessall[s, i].T)
+
+        return hessall
     #
     def coupling(self, gms, pairs = None):
         """

surrogate.py
@@ -152,6 +152,8 @@ class Adiabat(Surrogate):
             old = self.descriptors[st].shape[0]
             new = data[0][i,:,:].shape[0]
 
+            print(f"Update surrogate: state {st}, training data size={old+new}")
+
             self.descriptors[st].resize((old + new, d_size))
             self.training[st].resize((old + new))
 
@@ -163,7 +165,7 @@ class Adiabat(Surrogate):
 
             self.models[st].fit(self.descriptors[st],
                                 self.training[st])
-
+        print(f"Update surrogate: training data size={old+new}")
         return [model.kernel_.theta for model in self.models]
 
     #
@@ -302,34 +304,41 @@ class Adiabat(Surrogate):
         compute the hessian by gradient differences
         """
 
-        # if no specific states are requested, return all state
-        # energies
-        if states == None:
-            eval_st = [i for i in range(self.nstates)]
+        if states is None:
+            eval_st = list(range(self.nstates))
         else:
             eval_st = states
 
-        delta   = 0.0001
-        ng      = gms.shape[0]
-        nc      = gms.shape[1]
-        hessall = np.zeros((len(eval_st), ng, nc, nc), dtype=float)
+        delta = 1e-4
+        ng = gms.shape[0]  # number of geometries
+        nc = gms.shape[1]  # number of coordinates
+        nstates = len(eval_st)
+
+        hessall = np.zeros((nstates, ng, nc, nc), dtype=float)
 
         for i in range(ng):
+            # gms[i,:] shape: (nc,)
+            for k in range(nc):
+                # Prepare displaced geometries for plus and minus displacement
+                disp_plus = np.array(gms[i,:], copy=True)
+                disp_minus = np.array(gms[i,:], copy=True)
 
-            origin = np.tile(gms[i,:], (len(eval_st), nc))
+                disp_plus[k] += delta
+                disp_minus[k] -= delta
 
-            disps   = origin + np.diag(np.array([delta]*nc))
-            p_grad  = self.gradient(disps, states=eval_st)
+                # Get gradients at displaced points for all eval_st states
+                # Assuming self.gradient returns shape: (nstates, nc)
+                p_grad = self.gradient(disp_plus.reshape(1, -1), states=eval_st)  # shape (nstates, nc)
+                m_grad = self.gradient(disp_minus.reshape(1, -1), states=eval_st)  # shape (nstates, nc)
 
-            disps   = origin - np.diag(np.array([delta]*nc))
-            m_grad  = self.evaluate(disps, states=eval_st)
+                # Central difference to approximate second derivative w.r.t coordinate k
+                # For each state, calculate second derivative matrix element for k-th column
+                # hessall[:, i, :, k] = (p_grad - m_grad) / (2 * delta)
+                hessall[:, i, :, k] = (p_grad - m_grad) / (2 * delta)
 
-            hess    = (p_grad - m_grad ) / (2.*delta)
-
-            hess += hess.T
-            hess *= 0.5
-
-            hessall[:, i, :, :] = hess
+            # Symmetrize Hessian for each state and geometry
+            for s in range(nstates):
+                hessall[s, i] = 0.5 * (hessall[s, i] + hessall[s, i].T)
 
         return hessall
 