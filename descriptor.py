added bcm, fixed gradient bug

descriptor.py
@@ -53,24 +53,34 @@ class Soap(Descriptor):
         descriptors = []
         natm        = len(self.atoms)
 
-        for i in range(gms.shape[0]):
-            gm        = np.reshape(gms[i,:]*constants.bohr2ang,(natm,3))
+        if len(gms.shape) == 1:
+            eval_gms = np.array([gms], dtype=float)
+        else:
+            eval_gms = gms
+
+        for i in range(eval_gms.shape[0]):
+            gm        = np.reshape(
+                            eval_gms[i,:]*constants.bohr2ang,(natm,3))
             molecule  = Atoms(symbols=self.atoms, positions=gm)
             descriptor = self.generator.create(molecule)
             descriptors.append(descriptor/np.linalg.norm(descriptor))
 
         # return the geometries
-        return np.array(descriptors)
+        if len(gms.shape) == 1:
+            return np.array(descriptors[0])
+        else:
+            return np.array(descriptors)
 
-    def descriptor_gradient(self, gms, descriptor, delta=0.02):
+
+    def descriptor_gradient(self, gms, delta=0.02):
         """
         calculate gradient of SOAP descritor over cartesian coordinates
         """
         ng = gms.shape[0]
         nc = gms.shape[1]
 
-        n_feature = descriptor.shape[1]
-
+        descriptor = self.generate(gms[0,:])
+        n_feature  = descriptor.shape[0]
         # print(f'ng:{ng}')
         # print(f'nc:{nc}')
         # print(f'n_feature:{n_feature}')

gpr.py
@@ -6,7 +6,6 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from scipy.linalg import solve_triangular
 
 GPR_CHOLESKY_LOWER = True
-debug = False
 
 class GPRegressor(GaussianProcessRegressor):
     """
@@ -19,120 +18,157 @@ class GPRegressor(GaussianProcessRegressor):
         """
         super().__init__(**kwargs)
 
-    def _cal_kernel_gradient(self, X):
+    def _cal_kernel_gradient(self, xtest):
         """
         calculate kernel gradient (dk*/dx*) based on the give model 
         hyperparameters and input data
+
+        Xtest         = [Nfeatures]
+        X_train.shape = [Ntraining, Nfeatures]
+        kernel_gradie = [Nfeatures, NTraining]
         """
+        # this function assumes xtest is a single point
+        if len(xtest.shape) > 1:
+            os.abort('_cal_kernel_gradient assumes a single test pt.')
+
         # get hyper parameters
-        hyper_para   = np.exp(self.kernel_.theta)
-        length_scale = hyper_para[1]
+        hyper_para = np.exp(self.kernel_.theta)
+        con        = hyper_para[0]
+        len_scale  = hyper_para[1]
 
-        diff = X.copy() - self.X_train_.copy()
-        kernel_gradient = np.einsum('ai,a->ai', -(diff/length_scale**2), 
-                               self.kernel_(X, self.X_train_).squeeze())
+        Xq   = np.array([xtest], dtype=float)
+        diff = Xq - self.X_train_
 
-        # check if the kernel formula is consitent with the 
-        #built-in function
-        if debug:
-            print(diff.shape)
-            print(X.shape)
-            print(self.X_train_.shape)
-            print(self.alpha_.shape)
-            dists = cdist(X / length_scale, self.X_train_ / 
-                          length_scale, metric="sqeuclidean")
-            kernel = hyper_para[0]*np.exp(-0.5*dists)
-            kernel_test = self.kernel_(X, self.X_train_)
-            assert np.allclose(kernel, kernel_test)
-            os._exit(0)
+        kernel_gradient = np.einsum('ai,a->ai', 
+                          -con*(diff/len_scale**2), 
+                           self.kernel_(Xq, self.X_train_).squeeze())
 
         return kernel_gradient
 
-    def _cal_kernel_hessian(self, X):
+    def _cal_kernel_hessian(self, xtest):
         """
         calculate kernel hessian (dk*^2/dx*_idx_j) based on the give 
         model hyperparameters and input data
         """
-        # get hyper-parameters
-        hyper_para     = np.exp(self.kernel_.theta)
-        num_feature    = X.shape[1]
-        kernel_hessian = (hyper_para[0] / 
-                          hyper_para[1]**2) * np.eye(num_feature)
 
-        if debug: # print for debug
-            print(kernel.shape)
-            print(X.shape)
-            print(dists.shape)
-            print(dists)
-            print(kernel_hessian)
-            kernel_test = self.kernel_(X).copy()
-            print(kernel_test.shape)
-            print(kernel.shape)
-            assert np.allclose(kernel, kernel_test)
-            os._exit(0)
+        # kernel hessian assumes a single test point
+        # this function assumes xtest is a single point, returns
+        # [Nfeature, Nfeature] matrix
+        if len(xtest.shape) > 1:
+            os.abort('_cal_kernel_gradient assumes a single test pt.')
+        
+        # get hyper-parameters
+        hyper_para = np.exp(self.kernel_.theta)
+        con        = hyper_para[0]
+        len_scale  = hyper_para[1]
+
+        num_feature    = xtest.shape[0]
+        kernel_hessian = (con / len_scale**2) * np.eye(num_feature)
 
         return kernel_hessian
 
+    def kernel_gradient(self, X):
+        """
+        compute the derivative of the kernel matrix at query points X
+        """
 
-    def predict_grad(self, X, compute_grad_var=True):
+        # check if this is a single geometry, or as set of points
+        if len(X.shape) == 1:
+            ng   = 1
+            Xmat = np.array([X], dtype=float)
+        else:
+            ng   = X.shape[0]
+            Xmat = X
+
+        (nd, nt) = self.X_train_.shape
+        K_grads = np.zeros((ng, nd, nt), dtype=float)
+        for i in range(ng):
+            K_grads[i,:,:] = self._cal_kernel_gradient(Xmat[i])
+            
+        if len(X.shape)==1:
+            return K_grads[0,:,:]
+        else:
+            return K_grads
+
+    #
+    def kernel_hessian(self, X):
+        """
+        compute the derivative of the kernel matrix at query points X
+        """
+
+        # check if this is a single geometry, or as set of points
+        if len(X.shape) == 1:
+            ng   = 1
+            nd   = X.shape[0]
+            Xmat = np.array([X], dtype=float)
+        else:
+            ng   = X.shape[0]
+            nd   = X.shape[1]
+            Xmat = X
+
+        K_hess = np.zeros((ng, nd, nd), dtype=float)
+        for i in range(ng):
+            K_hess[i,:,:] = self._cal_kernel_hessian(Xmat[i,:])
+            K_hess[i,:,:] = np.squeeze(
+                                np.outer(K_hess[i,:,:],
+                                self._y_train_std**2).reshape(
+                                *(nd,nd), -1), axis=2)
+
+        if len(X.shape)==1:
+            return K_hess[0,:,:]
+        else:
+            return K_hess
+
+    #
+    def predict_grad(self, X, covariance=True):
         """Predict analytical gradient of the target function.
         """
         # calcuate analytical gradient of the target fucntion 
         # according 2.100 of McHutchon PhD thesis
-        kernel_gradient = self._cal_kernel_gradient(X)
+        if len(X.shape) > 1:
+            os.abort('predicting multiple gradients not currently supported')
 
-        y_grad = kernel_gradient.transpose() @ self.alpha_
+        kernel_gradient = self.kernel_gradient(X)
+        #print('kernel_gradient.shape='+str(kernel_gradient.shape))
+        #print('self.X_train.shape='+str(self.X_train_.shape))
+        #print('X.shape='+str(X.shape))
+        y_grad = np.transpose(kernel_gradient) @ self.alpha_
 
         # undo normalisation
-        y_grad = self._y_train_std * y_grad
+        y_grad *= self._y_train_std
 
-        # if we don't need to compute the variance,
+        # if we don't need to compute the co-variance,
         # exit here.
-        if not compute_grad_var:
+        if not covariance:
             return y_grad, None
 
-        kernel_hessian = self._cal_kernel_hessian(X)
+        kernel_hessian = self.kernel_hessian(X)
+        #print('kernel_hessian.shape='+str(kernel_hessian.shape))
 
         # using Cholesky decomposition of efficiently evaluate the 
         # second term in gradient variance expression
         V = solve_triangular(self.L_, kernel_gradient, 
                             lower=GPR_CHOLESKY_LOWER, 
                              check_finite=False)
-        temp = V.T @ V
+        #print('V.shape='+str(V.shape))
+        kKinvk = V.T @ V
 
         # calcuate gradient variance 
         # (in analogy of how to calcuate variance)
-        y_grad_var = kernel_hessian - temp
+        y_grad_covar = kernel_hessian - kKinvk
+
+        #print('y_grad_var.shape='+str(y_grad_var.shape))
 
         # undo normalization
-        y_grad_var = np.outer(y_grad_var, 
+        y_grad_covar = np.outer(y_grad_covar, 
                         self._y_train_std**2).reshape(
-                                    *y_grad_var.shape, -1)
+                                    *y_grad_covar.shape, -1)
+        #print('shape of unorm='+str(np.outer(y_grad_var,self._y_train_std**2).shape))
+        #print('y_grad_var.shape='+str(y_grad_var.shape))
 
         # if y_cov has shape (n_samples, n_samples, 1), 
         # reshape to (n_samples, n_samples)
-        if y_grad_var.shape[2] == 1:
-            y_grad_var = np.squeeze(y_grad_var, axis=2)
+        if y_grad_covar.shape[2] == 1:
+            y_grad_covar = np.squeeze(y_grad_covar, axis=2)
 
-        if debug: # print for debug
-            # check if Cholecky decomposition is performed correctly
-            K_test = self.kernel_(self.X_train_)
-            k_star = self.kernel_(X, self.X_train_)
-            K_test[np.diag_indices_from(K_test)] += self.alpha
-            K_inv_test = np.linalg.inv(K_test)
-            Y =  kernel_gradient.T @ K_inv_test @ kernel_gradient
-            print(Y.shape)
-            print(kernel_gradient.shape)
-            print(abs(Y).max())
-            print(abs(temp).max())
-            print(abs(Y-temp).max())
-            print(k_star.shape)
-            print(kernel_gradient.shape)
-            #assert np.allclose(Y, temp)
-
-            print(f'shape of kernel gradient: {kernel_gradient.shape}')
-            print(f'shape of V matrix:{V.shape}')
-            print(f'shape of kernel hessian: {kernel_hessian.shape}')
-            print(f'shape of gradient variance:{y_grad_var.shape}')
-
-        return y_grad, y_grad_var
+        return y_grad, y_grad_covar

surrogate.py
@@ -2,6 +2,7 @@
 The Surface ABC
 """
 import os
+import copy as copy
 from abc import ABC, abstractmethod
 import numpy as np
 import pickle as pickle
@@ -59,8 +60,17 @@ class Adiabat(Surrogate):
                        hparam=[10, 1]):
         super().__init__()
 
+        #print('self.kernel='+str(self.kernel))
+        self.ktype          = kernel
+        self.hparam         = hparam
+        self.nstates        = nstates
+        self.descriptor     = descriptor
+        self.models         = []
+        self.descriptors    = [[]]*nstates
+        self.training       = [[]]*nstates
+
         if kernel == 'RBF':
-            self.kernel = C(hparam[0], 
+            self.kernel = C(hparam[0],
                             constant_value_bounds=(1e-5, 1e5)) * \
                           RBF(hparam[1],
                             length_scale_bounds=(1e-3, 1e3))
@@ -73,12 +83,43 @@ class Adiabat(Surrogate):
             print('Kernel: '+str(kernel)+' not recognized.')
             os.abort()
 
-        #print('self.kernel='+str(self.kernel))
-        self.nstates        = nstates
-        self.descriptor     = descriptor
-        self.models         = []
-        self.descriptors    = [[]]*nstates
-        self.training       = [[]]*nstates
+        #for i in range(self.nstates):
+        #    gpregress = gpr.GPRegressor(
+        #                     kernel               = self.kernel,
+        #                     n_restarts_optimizer = 50,
+        #                     normalize_y          = True,
+        #                     optimizer            = 'fmin_l_bfgs_b')
+        #    self.models.append(gpregress)
+
+    #
+    def copy(self):
+        """
+        copy surrogate object
+        """
+
+        new = Adiabat(self.nstates, 
+                      self.descriptor, 
+                      kernel=self.ktype, 
+                      hparam=self.hparam)
+
+        var_dict = {key:value for key,value in self.__dict__.items()
+                   if not key.startswith('__') and not callable(key)}
+
+        for key, value in var_dict.items():
+            if hasattr(value, 'copy'):
+                setattr(new, key, value.copy())
+            else:
+                setattr(new, key, copy.deepcopy(value))
+
+        return new
+
+    #
+    def create(self, data, states=[], hparam=None, nrestart=None):
+        """
+        create a surrogate with training data, data
+        """
+
+        # create the regressor object
         for i in range(self.nstates):
             gpregress = gpr.GPRegressor(
                              kernel               = self.kernel,
@@ -86,12 +127,6 @@ class Adiabat(Surrogate):
                              normalize_y          = True,
                              optimizer            = 'fmin_l_bfgs_b')
             self.models.append(gpregress)
-
-    #
-    def create(self, data, states=[], hparam=None, nrestart=None):
-        """
-        create a surrogate with training data, data
-        """
 
         # sanity check the geometry array
         nst = len(states) 
@@ -106,7 +141,7 @@ class Adiabat(Surrogate):
             npt = [data[0].shape[0]]*nst
             same_geom = True
 
-        print('shape data[1]='+str(data[1].shape))
+        #print('shape data[1]='+str(data[1].shape))
         # if states is not given, but the data is for all
         # states, set the eval_states to be all states
         if nst == self.nstates and nst==0:
@@ -291,7 +326,7 @@ class Adiabat(Surrogate):
 
     #
     def gradient(self, gms, states=None, variance=False, 
-                 numerical=False):
+                 covariance=False, numerical=False):
         """
         evaluate the gradient using analytical expression
 
@@ -310,54 +345,65 @@ class Adiabat(Surrogate):
         else:
             eval_st = states
 
+        #print('self.descriptor[0]='+str(self.descriptors[0][0,:10]))
         # accept both a 1D array (single) geometry and a 2D array
         # (list of geometries)
         if len(gms.shape) == 2:
-            ngm      = gms.shape[0]
+            ng       = gms.shape[0]
+            nc       = gms.shape[1]
             eval_gms = gms
         elif len(gms.shape) == 1:
-            ngm      = 1
+            ng       = 1
+            nc       = gms.shape[0]
             eval_gms = np.array([gms], dtype=float)
         else:
             print('Cannot interprete gms array - surface.evaluate')
             os.abort()
 
+        #print('ngm='+str(ng))
         d_data   = self.descriptor.generate(eval_gms)
-        des_grad = self.descriptor.descriptor_gradient(eval_gms, d_data)
+        #print('d_data.shape='+str(d_data.shape))
+        #print('d_data.reshape(1,-1).shape='+str(d_data.reshape(1,-1).shape))
+        des_grad = self.descriptor.descriptor_gradient(eval_gms)
+        #print('des_grad.shape='+str(des_grad.shape))
 
-        ng = eval_gms.shape[0]
-        nc = eval_gms.shape[1]
-
-        ana_grad     = np.zeros((len(eval_st), ng, nc), dtype=float)
-        ana_grad_var = np.zeros_like(ana_grad)
+        grad       = np.zeros((len(eval_st), ng, nc), dtype=float)
+        grad_var   = np.zeros((len(eval_st), ng, nc), dtype=float)
+        grad_covar = np.zeros((len(eval_st), ng, nc, nc), dtype=float)
         for i in range(len(eval_st)):
             st = eval_st[i]
             for j in range(ng):
-                grad, grad_var_soap  = self.models[st].predict_grad(
-                                       d_data[j, :].reshape(1, -1),
-                                       compute_grad_var=variance)
-                grad = np.dot(des_grad[j,: ], grad).squeeze()
-                ana_grad[i, j, :] = grad
+                grad_d, grad_var_d  = self.models[st].predict_grad(
+                                      d_data[j, :],
+                                      covariance=True)
+                #print('grad_var_d.shape='+str(grad_var_d.shape))
+                #print('grad_d.shape='+str(grad_d.shape))
+                grad[i,j,:] = np.dot(des_grad[j,: ], grad_d).squeeze()
                 # print(f"analytical gradient:\n{grad.shape}")
-                if variance:
-                    #print('|descriptor_grad|='+str(np.linalg.norm(des_grad[j,:])))
-                    #print('|grad_var_soap|='+str(np.linalg.norm(grad_var_soap)))
-                    grad_var = np.diag(des_grad[j,:] @ grad_var_soap @ des_grad[j,:].T)
-                    ana_grad_var[i, j,: ] = grad_var
-                    #print('|grad_var|='+str(np.linalg.norm(grad_var)))
+                if covariance:
+                    grad_var_c = des_grad[j,:,:] @ grad_var_d @ des_grad[j,:,:].T
+                    #print('grad_var_c.shape='+str(grad_var_c.shape))
+                    grad_var[i, j, : ]      = np.diag(grad_var_c)
+                    grad_covar[i, j, :, :] = grad_var_c
 
+        #print('grad_d='+str(grad_d[:10]))
+        #print('grad='+str(grad[0,0,:]))
         # return a 2D array (nst, ncrd) if a single geometry is requested,
         # else return a 3D array (nst, ng, ncrd)
         if len(gms.shape) == 1:
+            vals = grad[:, 0, :]
             if variance:
-                return ana_grad[:,0,:], ana_grad_var[:,0,:]
-            else:
-                return ana_grad[:,0,:]
+                vals = (vals,) + (grad_var[:, 0, :],)
+            if covariance:
+                vals = (vals,) + (grad_covar[:, 0, :, :],)
         else:
+            vals = grad
             if variance:
-                return ana_grad, ana_grad_var
-            else:
-                return ana_grad
+                vals = (vals,) + (grad_var,)
+            if covariance:
+                vals = (vals,) + (grad_covar,)
+
+        return vals
 
     #
     def hessian(self, gms, states = None):
@@ -464,7 +510,8 @@ class Adiabat(Surrogate):
         o_ener    = self.evaluate(gms, eval_st)
         for i in range(ng):
 
-            origin = np.tile(gms[i,:], (len(eval_st), nc))
+            #origin = np.tile(gms[i,:], (len(eval_st), nc))
+            origin = np.tile(gms[i,:], (nc, len(eval_st)))
 
             disps   = origin + np.diag(np.array([delta]*nc))
             p_ener  = self.evaluate(disps, states=eval_st)